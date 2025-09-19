215d60fb657563a2b33d94f61d8b91589058c0ce
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-09-19 06:51:38 +0530

    Echo GET data

--- rest_framework/api/views.py
+++ rest_framework/api/views.py
@@ -1,3 +1,18 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def api_home(request, *args, **kwarg):
+    # JSON data -> request.body
+    body = request.body # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body) # converting byte data to json
+    except:
+        pass
+
+    data['params']=dict(request.GET) # Getting the query params
+    data['headers']=dict(request.headers) # Getting the headers
+    data['content_type']=request.content_type # Getting the content_type
+
+    return JsonResponse(data)
