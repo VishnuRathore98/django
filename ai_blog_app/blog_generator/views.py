dc271e39777e3faebc0e1ed9ea7961303841b2e3
VishnuRathore98 <rathorevishnu666@gmail.com>
2025-09-23 06:26:34 +0530

    user authentication

--- ai_blog_app/blog_generator/views.py
+++ ai_blog_app/blog_generator/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     return render(request, 'index.html') 
-
+    
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            error_message = 'username or password incorrect'
+            return render(request, 'login.html', {'error_message':error_message})
+
     return render(request, 'login.html')
 
 def user_signup(request):
@@ -32,4 +45,5 @@ def user_signup(request):
     return render(request, 'signup.html')
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect('/')
